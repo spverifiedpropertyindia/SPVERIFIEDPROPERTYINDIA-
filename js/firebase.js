6b43c9a0e23b2960d6a8c04704610be1753e12c5
spverifiedpropertyindia <raazsahu1000@gmail.com>
2026-01-25 15:33:59 +0530

    Refactor Firebase initialization and imports
    Updated Firebase imports to use CDN links and added authentication and Firestore functionalities.

--- js/firebase.js
+++ js/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDKyu_TDnbqE6yO9kx0pahFnxqL72q38ME",
   authDomain: "raazsahuteam-d02de.firebaseapp.com",
   projectId: "raazsahuteam-d02de",
   storageBucket: "raazsahuteam-d02de.firebasestorage.app",
   messagingSenderId: "307824931465",
-  appId: "1:307824931465:web:9f6de256e9d8eb7f528c58",
-  measurementId: "G-BG0SCBN118"
+  appId: "1:307824931465:web:9f6de256e9d8eb7f528c58"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
